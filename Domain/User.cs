a4702dc52afe5c49d65f8050044a64d8dcd1502d
Ra Zi <rasool.ziaei68@gmail.com>
2024-09-09 10:27:01 +0330

    develop infrastructure/BaseForm.cs & edit Domain/User

--- Domain/User.cs
+++ Domain/User.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace Domain
+namespace Domain;
+
+public class User(string firstName, string lastName, int nationalCod)
 {
-    internal class User
-    {
-    }
+    [Browsable(browsable: false)]
+    [DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.None)]
+    public Guid Id { get; protected set; } = Guid.NewGuid();
+
+    [Required(AllowEmptyStrings = false)]
+    [StringLength(maximumLength: 25, MinimumLength = 3)]
+    public string FirstName { get; set; } = firstName;
+
+    [Required(AllowEmptyStrings = false)]
+    [StringLength(maximumLength: 25, MinimumLength = 3)]
+    public string LastName { get; set; } = lastName;
+
+    [Required(AllowEmptyStrings = false)]
+    [StringLength(maximumLength: 10, MinimumLength = 10)]
+    public int NationalCod { get; set; } = nationalCod;
+
+    [Browsable(browsable: false)]
+    public string? Address { get; set; }
+
 }
--- Persistence/ApplicationDbContext.cs
+++ Persistence/ApplicationDbContext.cs
@@ -1,9 +1,37 @@
-﻿
+﻿using Domain;
 using Microsoft.EntityFrameworkCore;
 
-namespace Persistence
+namespace Persistence;
+
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext()
     {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // *** Windows Authentication Mode
+        var connectionString =
+            "Server=.;" +
+            "Database = Mini_Project;" +
+            "MultipleActiveResultSets = true;" +
+            "Trusted_Connection = True;" +
+            "TrustServerCertificate = True; ";
+
+        // *** SQL Server Authentication Mode
+        //var connectionString =
+        //    "Server=.;" +
+        //    "User ID=sa;" +
+        //    "Password=1234512345;" +
+        //    "Database=Mini_Project;" +
+        //    "MultipleActiveResultSets=true;" +
+        //    "TrustServerCertificate=True;";
+
+        optionsBuilder.UseSqlServer(connectionString: connectionString);
+
     }
 }
