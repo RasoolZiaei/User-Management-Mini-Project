5f369a3cc1eaa5be7a9c2a726f2df8b558c0b16e
Ra Zi <rasool.ziaei68@gmail.com>
2024-09-09 12:47:45 +0330

    Develop minor feature of main form

--- MyApplication/MainForm.Designer.cs
+++ MyApplication/MainForm.Designer.cs
@@ -28,96 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Razi.windows.Forms.Panel();
-            button2 = new Razi.windows.Forms.Button();
-            textBox1 = new Razi.windows.Forms.TextBox();
-            label1 = new Razi.windows.Forms.Label();
-            button1 = new Razi.windows.Forms.Button();
-            dataGridView1 = new Razi.windows.Forms.DataGridView();
-            panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            mainPanel = new Razi.windows.Forms.Panel();
+            searchButton = new Razi.windows.Forms.Button();
+            searchTextBox = new Razi.windows.Forms.TextBox();
+            fullNameLabel = new Razi.windows.Forms.Label();
+            addUserButton = new Razi.windows.Forms.Button();
+            userDataGridView = new Razi.windows.Forms.DataGridView();
+            mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)userDataGridView).BeginInit();
             SuspendLayout();
             // 
-            // panel1
+            // mainPanel
             // 
-            panel1.Controls.Add(button2);
-            panel1.Controls.Add(textBox1);
-            panel1.Controls.Add(label1);
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(dataGridView1);
-            panel1.Dock = DockStyle.Fill;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(578, 300);
-            panel1.TabIndex = 4;
+            mainPanel.Controls.Add(searchButton);
+            mainPanel.Controls.Add(searchTextBox);
+            mainPanel.Controls.Add(fullNameLabel);
+            mainPanel.Controls.Add(addUserButton);
+            mainPanel.Controls.Add(userDataGridView);
+            mainPanel.Dock = DockStyle.Fill;
+            mainPanel.Location = new Point(0, 0);
+            mainPanel.Name = "mainPanel";
+            mainPanel.Size = new Size(664, 361);
+            mainPanel.TabIndex = 0;
             // 
-            // button2
+            // searchButton
             // 
-            button2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            button2.Location = new Point(491, 41);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 7;
-            button2.Text = "&Search";
-            button2.UseVisualStyleBackColor = true;
+            searchButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            searchButton.Location = new Point(577, 41);
+            searchButton.Name = "searchButton";
+            searchButton.Size = new Size(75, 23);
+            searchButton.TabIndex = 3;
+            searchButton.Text = "&Search";
+            searchButton.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // searchTextBox
             // 
-            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            textBox1.Location = new Point(79, 41);
-            textBox1.MaxLength = 50;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(406, 23);
-            textBox1.TabIndex = 6;
+            searchTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            searchTextBox.Location = new Point(79, 41);
+            searchTextBox.MaxLength = 50;
+            searchTextBox.Name = "searchTextBox";
+            searchTextBox.Size = new Size(492, 23);
+            searchTextBox.TabIndex = 2;
             // 
-            // label1
+            // fullNameLabel
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 44);
-            label1.Name = "label1";
-            label1.Size = new Size(61, 15);
-            label1.TabIndex = 5;
-            label1.Text = "&Full Name";
+            fullNameLabel.AutoSize = true;
+            fullNameLabel.Location = new Point(12, 44);
+            fullNameLabel.Name = "fullNameLabel";
+            fullNameLabel.Size = new Size(61, 15);
+            fullNameLabel.TabIndex = 1;
+            fullNameLabel.Text = "&Full Name";
             // 
-            // button1
+            // addUserButton
             // 
-            button1.Location = new Point(12, 12);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 4;
-            button1.Text = "&Add User";
-            button1.UseVisualStyleBackColor = true;
+            addUserButton.Location = new Point(12, 12);
+            addUserButton.Name = "addUserButton";
+            addUserButton.Size = new Size(69, 23);
+            addUserButton.TabIndex = 0;
+            addUserButton.Text = "&Add User";
+            addUserButton.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // userDataGridView
             // 
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(0, 70);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(578, 230);
-            dataGridView1.TabIndex = 0;
+            userDataGridView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            userDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            userDataGridView.Location = new Point(0, 70);
+            userDataGridView.Name = "userDataGridView";
+            userDataGridView.Size = new Size(664, 291);
+            userDataGridView.TabIndex = 4;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(578, 300);
-            Controls.Add(panel1);
+            ClientSize = new Size(664, 361);
+            Controls.Add(mainPanel);
+            MinimumSize = new Size(680, 400);
             Name = "MainForm";
             Text = "Main";
             Load += MainForm_Load;
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            mainPanel.ResumeLayout(false);
+            mainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)userDataGridView).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-        private Razi.windows.Forms.Panel panel1;
-        private Razi.windows.Forms.DataGridView dataGridView1;
-        private Razi.windows.Forms.Button button2;
-        private Razi.windows.Forms.TextBox textBox1;
-        private Razi.windows.Forms.Label label1;
-        private Razi.windows.Forms.Button button1;
+        private Razi.windows.Forms.Panel mainPanel;
+        private Razi.windows.Forms.DataGridView userDataGridView;
+        private Razi.windows.Forms.Button searchButton;
+        private Razi.windows.Forms.TextBox searchTextBox;
+        private Razi.windows.Forms.Label fullNameLabel;
+        private Razi.windows.Forms.Button addUserButton;
     }
 }
